a9ed4c5aebb19d80cad9d5c7c8c2bcdce82d2361
Theanh0411 <theanh@mogul.sg>
2023-02-25 20:17:00 +0700

    fix: add static page

--- src/app.js
+++ src/app.js
@@ -53,6 +53,12 @@ if (config.env === 'production') {
 // v1 api routes
 app.use('/v1', routes);
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
+
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
